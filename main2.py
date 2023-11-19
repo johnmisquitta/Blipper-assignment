3a7e26d7280bd36935f039a84344c2b55cd46051
johnmisquitta <34851009+johnmisquitta@users.noreply.github.com>
2023-11-19 11:59:03 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,1 +1,8 @@
 
+import streamlit as st
+
+
+st.header("Pick an Audio File")
+uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav", "ogg"]) #upload file
+st.audio(uploaded_file, format='audio/wav') #display audio
+st.write("<hr>", unsafe_allow_html=True) 
