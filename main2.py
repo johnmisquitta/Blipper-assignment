fd70849cd485c17639c698d2e4c7483555352b93
johnmisquitta <34851009+johnmisquitta@users.noreply.github.com>
2023-11-22 00:34:41 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,30 +1,114 @@
-!pip install speech_recognition
-
 import streamlit as st
-import speech_recognition as sr
+import numpy as np
 
-# Initialize the speech recognizer
-r = sr.Recognizer()
+# Define Streamlit app layout
+st.set_page_config(page_title="Speech Recognition Demo", page_icon="🔊")
 
-def recognize_speech():
-    try:
-        # Record audio from the microphone
-        with sr.Microphone() as source:
-            audio = r.listen(source)
+# Streamlit app styles
+st.markdown(
+    """
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            min-width: 100%;
+            min-height: 100vh;
+            font-family: sans-serif;
+            text-align: center;
+            color: #fff;
+            background: #000;
+        }
+        button {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 5em;
+            height: 2em;
+            margin-left: -2.5em;
+            margin-top: -1em;
+            z-index: 100;
+            padding: .25em .5em;
+            color: #fff;
+            background: #000;
+            border: 1px solid #fff;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 1.15em;
+            font-weight: 200;
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+            transition: box-shadow .5s;
+        }
+        button:hover {
+            box-shadow: 0 0 30px 5px rgba(255, 255, 255, 0.75);
+        }
+        main {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        main > div {
+            display: inline-block;
+            width: 3px;
+            height: 100px;
+            margin: 0 7px;
+            background: currentColor;
+            transform: scaleY(.5);
+            opacity: .25;
+        }
+        main.error {
+            color: #f7451d;
+            min-width: 20em;
+            max-width: 30em;
+            margin: 0 auto;
+            white-space: pre-line;
+        }
+        #transcript {
+            position: fixed;
+            top: 60%;
+            width: 100vw;
+            padding-left: 20vw;
+            padding-right: 20vw;
+            font-size: x-large;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-        # Recognize the speech
-        transcription = r.recognize_google(audio)
-
-        # Update the transcription display
-        st.write("Transcription:", transcription)
-    except sr.UnknownValueError:
-        st.write("Could not understand audio")
-    except sr.RequestError as e:
-        st.write(f"Could not request results from Google Speech Recognition service; {e}")
-
-# Create the Streamlit app
+# Streamlit app content
 st.title("Speech Recognition Demo")
 
-# Add a button to start recognition
-if st.button("Start Recognition"):
-    recognize_speech()
+# Create Streamlit button and transcript div
+if st.button("Start"):
+    st.markdown('<div id="transcript"></div>', unsafe_allow_html=True)
+
+    class AudioVisualizer:
+        def __init__(self):
+            self.visual_value_count = 16
+            self.visual_elements = None
+            self.data_map = {0: 15, 1: 10, 2: 8, 3: 9, 4: 6, 5: 5, 6: 2, 7: 1, 8: 0, 9: 4, 10: 3, 11: 7, 12: 11, 13: 12,
+                             14: 13, 15: 14}
+            self.init_visual_elements()
+
+        def init_visual_elements(self):
+            self.visual_elements = st.empty()
+            for _ in range(self.visual_value_count):
+                self.visual_elements.div(width=3, height=100, background_color="currentColor", style="transform: scaleY(.5); opacity: .25; margin: 0 7px;")
+
+        def process_frame(self, data):
+            values = np.array(list(data.values()))
+            for i in range(self.visual_value_count):
+                value = values[self.data_map[i]] / 255
+                st.markdown(
+                    f'<style> .st-eb {{ transform: scaleY({value}); opacity: {max(.25, value)}; }} </style>',
+                    unsafe_allow_html=True)
+
+    audio_visualizer = AudioVisualizer()
+
+    # TODO: Implement fetching audio data and updating the transcript using st.markdown
+    # Example usage:
+    # st.markdown("Updating transcript dynamically")
+
+# You may need to implement the actual logic for fetching audio data and updating the transcript using Streamlit's components.
+# The provided example is a starting point and needs to be extended based on your specific requirements.
